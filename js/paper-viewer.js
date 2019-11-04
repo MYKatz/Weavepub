c316dcfb11a66bf80976574c2cc08aa9a2045bc4
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2019-11-03 18:11:32 -0800

    Added v-for to paper viewer

--- js/paper-viewer.js
+++ js/paper-viewer.js
@@ -1,3 +1,27 @@
+Vue.component('Paper', {
+    template: `
+    <div class="col-md-12 col-lg-4">
+        <div class="card mb-4">
+            <div class="card-body">
+                <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
+                <p class="card-authors">{{authorsAsString}}</p>
+                <p class="card-text">{{shortAbstract}}</p>
+                 <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
+            </div>
+        </div>
+    </div>
+    `,
+    props: ['title', 'authors', 'abstract'],
+    computed: {
+        authorsAsString: function() {
+            return this.authors.join(", ");
+        },
+        shortAbstract: function() {
+            return this.abstract.length >= 200 ? this.abstract.slice(0, 200) + "..." : this.abstract;
+        }
+    }
+});
+
 Vue.component('PaperViewer', {
     template: `
     <div class="paper-viewer">
@@ -7,100 +31,26 @@ Vue.component('PaperViewer', {
             <div class="py-4">
                 <div class="row">
                     <div class="card-deck">
-                        <div class="col-md-12 col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
-                                    <p class="card-authors">Louis Deslauriers, Carl Wieman</p>
-                                    <p class="card-text">We compared the amounts of learning achieved using two
-                                        different instructional approaches
-                                        under controlled conditions. We measured the learning of a specific set of
-                                        topics and
-                                        objectives when...</p>
-                                    <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-12 col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
-                                    <p class="card-authors">Louis Deslauriers, Carl Wieman</p>
-                                    <p class="card-text">We compared the amounts of learning achieved using two
-                                        different instructional approaches
-                                        under controlled conditions. We measured the learning of a specific set of
-                                        topics and
-                                        objectives when...</p>
-                                    <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-12 col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
-                                    <p class="card-authors">Louis Deslauriers, Carl Wieman</p>
-                                    <p class="card-text">We compared the amounts of learning achieved using two
-                                        different instructional approaches
-                                        under controlled conditions. We measured the learning of a specific set of
-                                        topics and
-                                        objectives when...</p>
-                                    <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-12 col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
-                                    <p class="card-authors">Louis Deslauriers, Carl Wieman</p>
-                                    <p class="card-text">We compared the amounts of learning achieved using two
-                                        different instructional approaches
-                                        under controlled conditions. We measured the learning of a specific set of
-                                        topics and
-                                        objectives when...</p>
-                                    <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-12 col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
-                                    <p class="card-authors">Louis Deslauriers, Carl Wieman</p>
-                                    <p class="card-text">We compared the amounts of learning achieved using two
-                                        different instructional approaches
-                                        under controlled conditions. We measured the learning of a specific set of
-                                        topics and
-                                        objectives when...</p>
-                                    <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-12 col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <h4 class="card-title">Improved Learning in a Large-Enrollment Physics Class</h4>
-                                    <p class="card-authors">Louis Deslauriers, Carl Wieman</p>
-                                    <p class="card-text">We compared the amounts of learning achieved using two
-                                        different instructional approaches
-                                        under controlled conditions. We measured the learning of a specific set of
-                                        topics and
-                                        objectives when...</p>
-                                    <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                                </div>
-                            </div>
-                        </div>
+                        <Paper v-for="paper in papers" :title="paper.title" :authors="paper.authors" :abstract="paper.abstract" :key="paper.id"></Paper>
                     </div>
                 </div>
             </div>
         </div>
         </div>
     </div>
-    `
+    `,
+    data: function() {
+        var obj = {};
+        obj.papers = [];
+        for (let i = 0; i < 6; ++i) {
+            obj.papers.push({
+                title: "Improved Learning in a Large-Enrollment Physics Class",
+                authors: ["Louis Deslauriers", "Carl Wieman"],
+                abstract: `We compared the amounts of learning achieved using two different instructional approaches
+                under controlled conditions. We measured the learning of a specific set of topics and objectives when...`,
+                id: i
+            });
+        }
+        return obj;
+    }
 });
